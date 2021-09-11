88fb5fc6328b483ad8db217b62866d8c6f9a55f1
rosa-paula <rosalemos.moreira@gmail.com>
2021-09-11 20:07:18 -0300

    projeto dio

---
+++ DIO.Series/Classes/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Series
+{
+    class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
